9f6a6e54e7be0e40c88fdd656c2ee5000dbeea1e
tanmay3011 <tanmay3011@gmail.com>
2014-07-14 14:35:16 +0530

    Add autocommit to transaction

--- transaction/transaction.sql
+++ transaction/transaction.sql
@@ -1,5 +1,6 @@
 -- i) userA is depositing 1000 Rs. his account
 START TRANSACTION;
+set autocommit = 0;
 UPDATE accounts SET balance = balance + 1000
 WHERE account_no = (SELECT account_no FROM users WHERE id = 1);;
 COMMIT;
@@ -7,6 +8,7 @@ COMMIT;
 
 -- ii) userA is withdrawing 500 Rs.
 START TRANSACTION;
+set autocommit = 0;
 UPDATE accounts SET balance = balance - 500
 WHERE account_no = (SELECT account_no FROM users WHERE id = 1);
 COMMIT;
@@ -14,6 +16,7 @@ COMMIT;
 
 -- iii) userA is transferring 200 Rs to userB account
 START TRANSACTION;
+set autocommit = 0;
 UPDATE accounts SET balance = balance - 200
 WHERE account_no = (SELECT account_no FROM users WHERE id = 1);
 
