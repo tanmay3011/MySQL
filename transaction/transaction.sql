a047df5645d278909393026699714b810aa21e14
tanmay3011 <tanmay3011@gmail.com>
2014-07-14 13:48:52 +0530

    Initial Commit transaction Exercise

---
+++ transaction/transaction.sql
@@ -0,0 +1,22 @@
+-- i) userA is depositing 1000 Rs. his account
+START TRANSACTION;
+UPDATE accounts SET balance = balance + 1000
+WHERE account_no = (SELECT account_no FROM users WHERE id = 1);;
+COMMIT;
+
+
+-- ii) userA is withdrawing 500 Rs.
+START TRANSACTION;
+UPDATE accounts SET balance = balance - 500
+WHERE account_no = (SELECT account_no FROM users WHERE id = 1);
+COMMIT;
+
+
+-- iii) userA is transferring 200 Rs to userB account
+START TRANSACTION;
+UPDATE accounts SET balance = balance - 200
+WHERE account_no = (SELECT account_no FROM users WHERE id = 1);
+
+UPDATE accounts SET balance = balance + 200
+WHERE account_no = (SELECT account_no FROM users WHERE id = 2);
+COMMIT;
