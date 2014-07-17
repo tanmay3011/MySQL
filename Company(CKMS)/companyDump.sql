0c0b3c835781eef0dfdaa070c3fa637e0d3416b2
tanmay3011 <tanmay3011@gmail.com>
2014-07-17 16:59:48 +0530

    change datatype, correct naming issue

--- Company(CKMS)/companyDump.sql
+++ Company(CKMS)/companyDump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: ckms
+-- Host: localhost    Database: company
 -- ------------------------------------------------------
 -- Server version	5.5.34-0ubuntu0.13.04.1
 
@@ -23,10 +23,12 @@ DROP TABLE IF EXISTS `project_techs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `project_techs` (
-  `id` bigint(20) DEFAULT NULL,
-  `technology_id` bigint(20) DEFAULT NULL,
+  `project_id` bigint(20) unsigned DEFAULT NULL,
+  `technology_id` bigint(20) unsigned DEFAULT NULL,
+  KEY `project_id` (`project_id`),
   KEY `technology_id` (`technology_id`),
-  CONSTRAINT `project_techs_ibfk_1` FOREIGN KEY (`technology_id`) REFERENCES `technologies` (`id`)
+  CONSTRAINT `project_techs_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `project_techs_ibfk_2` FOREIGN KEY (`technology_id`) REFERENCES `technologies` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -36,7 +38,6 @@ CREATE TABLE `project_techs` (
 
 LOCK TABLES `project_techs` WRITE;
 /*!40000 ALTER TABLE `project_techs` DISABLE KEYS */;
-INSERT INTO `project_techs` VALUES (1,1),(1,2),(1,3),(1,4),(2,5),(3,6),(4,5),(4,6),(5,3),(5,4),(6,1),(6,2),(6,6),(7,6),(7,5),(8,1),(8,2),(8,3),(8,4),(8,6),(9,5);
 /*!40000 ALTER TABLE `project_techs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,10 +49,11 @@ DROP TABLE IF EXISTS `projects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `projects` (
-  `id` bigint(20) NOT NULL DEFAULT '0',
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `status` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +62,6 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'Completed'),(2,'Completed'),(3,'Completed'),(4,'Completed'),(5,'Working'),(6,'Working'),(7,'Completed'),(8,'Completed'),(9,'Completed');
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,10 +73,11 @@ DROP TABLE IF EXISTS `technologies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `technologies` (
-  `id` bigint(20) NOT NULL DEFAULT '0',
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,7 +86,6 @@ CREATE TABLE `technologies` (
 
 LOCK TABLES `technologies` WRITE;
 /*!40000 ALTER TABLE `technologies` DISABLE KEYS */;
-INSERT INTO `technologies` VALUES (1,'HTML'),(2,'Javascript'),(3,'Ruby'),(4,'Rails'),(5,'IOS'),(6,'Android');
 /*!40000 ALTER TABLE `technologies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,8 +97,8 @@ DROP TABLE IF EXISTS `user_projects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_projects` (
-  `user_id` bigint(20) DEFAULT NULL,
-  `project_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `project_id` bigint(20) unsigned DEFAULT NULL,
   KEY `user_id` (`user_id`),
   KEY `project_id` (`project_id`),
   CONSTRAINT `user_projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
@@ -111,7 +112,6 @@ CREATE TABLE `user_projects` (
 
 LOCK TABLES `user_projects` WRITE;
 /*!40000 ALTER TABLE `user_projects` DISABLE KEYS */;
-INSERT INTO `user_projects` VALUES (1,1),(1,2),(1,3),(1,6),(2,1),(2,3),(2,7),(2,9),(3,1),(3,2),(3,8),(3,9),(4,1),(4,2),(4,4),(4,5),(4,6);
 /*!40000 ALTER TABLE `user_projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,10 +123,11 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL DEFAULT '0',
-  `name` varchar(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,7 +136,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'UserA'),(2,'UserB'),(3,'UserC'),(4,'UserD');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -148,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-16  1:23:11
+-- Dump completed on 2014-07-17 16:56:05
--- Company(CKMS)/company_knowledge.sql
+++ Company(CKMS)/company_knowledge.sql
@@ -1,11 +1,11 @@
 -- 1) Find names of all employees currently not working in any projects. (Use joins)
-SELECT u.id
-FROM user_projects JOIN projects p
-ON user_projects.project_id = p.id
-AND p.status = 'Working'
-RIGHT JOIN users u
-ON user_projects.user_id = u.id
-WHERE p.status IS NULL;
+SELECT users.id
+FROM user_projects JOIN projects
+ON user_projects.project_id = projects.id
+AND projects.status = 'Working'
+RIGHT JOIN users
+ON user_projects.user_id = users.id
+WHERE projects.status IS NULL;
 
 /*
 +----+
@@ -18,12 +18,12 @@ WHERE p.status IS NULL;
 */
 
 -- 2) Find all employees who have exposure to HTML, Javascript and IOS.
-SELECT u.user_id user
-FROM user_projects u JOIN project_techs p 
-JOIN technologies t 
-WHERE t.name IN ('IOS','Javascript', 'HTML')
+SELECT user_projects.user_id user
+FROM user_projects JOIN project_techs ON user_projects.project_id = project_techs.project_id
+JOIN technologies ON project_techs.technology_id = technologies.id
+WHERE technologies.name IN ('IOS','Javascript', 'HTML')
 GROUP BY user
-HAVING COUNT(DISTINCT t.name) >=3;
+HAVING COUNT(DISTINCT technologies.name)>=3;
 
 /*
 +------+
@@ -38,40 +38,29 @@ HAVING COUNT(DISTINCT t.name) >=3;
 */
 
 -- 3) Find the technologies in which a particular employee(Say B) has expertise(3 or more projects)
-SELECT t.name FROM users u
-JOIN user_projects ps 
-JOIN project_techs pt 
-JOIN technologies t
-WHERE u.name = "userB"
-GROUP BY t.id
-HAVING count(t.id)>=3;
+SELECT technologies.name FROM users
+JOIN user_projects ON user_projects.user_id = users.id
+JOIN project_techs ON user_projects.project_id = project_techs.project_id
+JOIN technologies ON project_techs.technology_id = technologies.id
+WHERE users.name = "userB"
+GROUP BY technologies.id
+HAVING count(technologies.id)>=3;
 
 /*
-+------------+
-| name       |
-+------------+
-| HTML       |
-| Javascript |
-| Ruby       |
-| Rails      |
-| IOS        |
-| Android    |
-+------------+
-6 rows in set (0.00 sec)
+Empty set (0.00 sec)
 */
 
 -- 4) Find the employee who has done most no of projects in android
-SELECT u.user_id 
-FROM user_projects u
-JOIN project_techs pt 
-JOIN technologies t 
-WHERE t.name = "Android"
-HAVING count(u.user_id) = (
-  SELECT count(u.user_id) count
-  FROM user_projects u
-  JOIN project_techs pt 
-  JOIN technologies t 
-  WHERE t.name = "Android"
+SELECT user_projects.user_id FROM user_projects 
+JOIN project_techs ON user_projects.project_id = project_techs.project_id
+JOIN technologies ON project_techs.technology_id = technologies.id
+WHERE technologies.name = "Android"
+HAVING count(user_projects.user_id) = (
+  SELECT count(user_projects.user_id) count
+  FROM user_projects
+  JOIN project_techs ON user_projects.project_id = project_techs.project_id
+  JOIN technologies ON project_techs.technology_id = technologies.id
+  WHERE technologies.name = "Android"
   ORDER BY count DESC
   LIMIT 1
 );
